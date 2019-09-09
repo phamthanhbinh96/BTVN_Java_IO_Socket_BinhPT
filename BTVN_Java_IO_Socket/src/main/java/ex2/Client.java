2aa418ea4458ef4857eef08b9869183b1170f2cf
phamthanhbinh96 <54532614+phamthanhbinh96@users.noreply.github.com>
2019-09-09 23:47:25 +0700

    Update Client.java

--- BTVN_Java_IO_Socket/src/main/java/ex2/Client.java
+++ BTVN_Java_IO_Socket/src/main/java/ex2/Client.java
@@ -22,65 +22,63 @@ public class Client {
     	Convert c=new Convert();
         Socket socket;
         int port=9669;
-		try {
-			
-			socket = new Socket("localhost", port);
-			System.out.println("Connected!");
-	        DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
-	        DataInputStream dis = new DataInputStream(socket.getInputStream());
+        try {
+            socket = new Socket("localhost", port);
+            System.out.println("Connected!");
+            DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+            DataInputStream dis = new DataInputStream(socket.getInputStream());
 
-	        Scanner input = new Scanner(System.in);
-	        boolean ans=true;
-	        while(ans) {
-	        	System.out.println("---Request---");
-    	        List<Content> tvl=new ArrayList<Content>();
-	            String str= input.nextLine();
-	            String[] temp=str.split(" ");
-	            short cmdCode=c.convertCmdCodeToShort(temp[0]);
-	            if(cmdCode==4) {
-	            	System.out.println("Message Format is incorrect. (cmdCode+[Tag + value])");
-	            	continue;
-	            }
-	            for(int i=1;i<temp.length;i+=2) {
-	            	
-	            	tvl.add(new Content(c.convertTagToShort(temp[i]),temp[i+1]));
-	            }
-	           Message request=new Message(cmdCode,tvl);
+            Scanner input = new Scanner(System.in);
+            boolean ans=true;
+            while(ans) {
+                System.out.println("---Request---");
+                List<Content> tvl=new ArrayList<Content>();
+                String str= input.nextLine();
+                String[] temp=str.split(" ");
+                short cmdCode=c.convertCmdCodeToShort(temp[0]);
+                if(cmdCode==4) {
+                    System.out.println("Message Format is incorrect. (cmdCode+[Tag + value])");
+                    continue;
+                }
+                for(int i=1;i<temp.length;i+=2) {
 
-	           byte[] data=c.convertMessageToByteArray(request);
-	          
-	           dos.writeInt(data.length);
-	           dos.write(data);
-	           System.out.println("---Response---");
-	           int length=dis.readInt();
-	           byte[] messageByte = new byte[length];
-			   dis.readFully(messageByte, 0, messageByte.length);
-			   Message response=c.convertByteArrayToMessage(messageByte); 
-			   System.out.print(c.convertShortToCmdCode(response.getCmdCode())+" ");
-			   for(Content cont: response.getListTVL()) {
-				   System.out.print(c.convertShortToTag(cont.getTags())+" " + cont.getValue()+"\n");
-			   }
-			   System.out.println("Do you want to continue ?(y/n)");
-			   String a=input.next();
-			   input.nextLine();
-			   if(a.equalsIgnoreCase("y")) {
-				   ans=true;
-			   }else {
-				   ans=false;
-			   }
-	           
-	        }
-	        input.close();
-	        dos.close();
-	        dis.close();
-	        socket.close();
-		} catch (UnknownHostException e) {
-			System.out.println("Host is invalid");
-			e.printStackTrace();
-		} catch (IOException e) {
-			System.out.println("Accept failed!");
-			e.printStackTrace();
-		}
-        
+                    tvl.add(new Content(c.convertTagToShort(temp[i]),temp[i+1]));
+                }
+                Message request=new Message(cmdCode,tvl);
+
+                byte[] data=c.convertMessageToByteArray(request);
+
+                dos.writeInt(data.length);
+                dos.write(data);
+                System.out.println("---Response---");
+                int length=dis.readInt();
+                byte[] messageByte = new byte[length];
+                dis.readFully(messageByte, 0, messageByte.length);
+                Message response=c.convertByteArrayToMessage(messageByte); 
+                System.out.print(c.convertShortToCmdCode(response.getCmdCode())+" ");
+                for(Content cont: response.getListTVL()) {
+                    System.out.print(c.convertShortToTag(cont.getTags())+" " + cont.getValue()+"\n");
+                }
+                System.out.println("Do you want to continue ?(y/n)");
+                String a=input.next();
+                input.nextLine();
+                if(a.equalsIgnoreCase("y")) {
+                        ans=true;
+                }else {
+                        ans=false;
+                }
+
+            }
+            input.close();
+            dos.close();
+            dis.close();
+            socket.close();
+        } catch (UnknownHostException e) {
+                System.out.println("Host is invalid");
+                e.printStackTrace();
+        } catch (IOException e) {
+                System.out.println("Accept failed!");
+                e.printStackTrace();
+        }      
     }
 }

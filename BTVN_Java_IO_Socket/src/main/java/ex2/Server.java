3713228e57117dda4c9ac0a66480f8a36b44c0a1
phamthanhbinh96 <54532614+phamthanhbinh96@users.noreply.github.com>
2019-09-09 23:52:05 +0700

    Update Server.java

--- BTVN_Java_IO_Socket/src/main/java/ex2/Server.java
+++ BTVN_Java_IO_Socket/src/main/java/ex2/Server.java
@@ -20,29 +20,29 @@ import java.util.List;
 public class Server {
 	
 	
-	public static void main(String[] args)  {
-		ServerSocket server;
-		Socket socket=null;
-		try {
-			server = new ServerSocket(9669);
-			System.out.println("Waiting for client on port 9669");
-			//accept multi-client connect to this serversocket
-			while(true) {
-				socket = server.accept();
-				System.out.println("Client accepted");
-				DataInputStream dis = new DataInputStream(socket.getInputStream());
-				DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
-				Thread thread= new ClientHandler(dis,dos,socket);
-				thread.start();
-			}
-		} catch (IOException e) {
-			try {
-				socket.close();
-			} catch (IOException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-			e.printStackTrace();
-		}
-	}	
+    public static void main(String[] args)  {
+        ServerSocket server;
+        Socket socket=null;
+        try {
+            server = new ServerSocket(9669);
+            System.out.println("Waiting for client on port 9669");
+            //accept multi-client connect to this serversocket
+            while(true) {
+                socket = server.accept();
+                System.out.println("Client accepted");
+                DataInputStream dis = new DataInputStream(socket.getInputStream());
+                DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+                Thread thread= new ClientHandler(dis,dos,socket);
+                thread.start();
+            }
+        } catch (IOException e) {
+            try {
+                    socket.close();
+            } catch (IOException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+            }
+            e.printStackTrace();
+        }
+    }	
 }

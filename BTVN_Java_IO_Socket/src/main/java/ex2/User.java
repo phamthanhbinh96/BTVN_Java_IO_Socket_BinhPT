18df8ec45f5cf9faa5dd6ec9de90f9febad04907
phamthanhbinh96 <54532614+phamthanhbinh96@users.noreply.github.com>
2019-09-09 23:52:42 +0700

    Update User.java

--- BTVN_Java_IO_Socket/src/main/java/ex2/User.java
+++ BTVN_Java_IO_Socket/src/main/java/ex2/User.java
@@ -10,41 +10,41 @@ package ex2;
  * @author P.T.B
  */
 public class User {
-	private String name;
-	private String phoneNumber;
-	private Status st;
-	public User(String name, String phoneNumber, Status st) {
-		super();
-		this.name = name;
-		this.phoneNumber = phoneNumber;
-		this.st = st;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-	public Status getSt() {
-		return st;
-	}
-	public void setSt(Status st) {
-		this.st = st;
-	}
-	public User(String name, String phoneNumber) {
-		super();
-		this.name = name;
-		this.phoneNumber = phoneNumber;
-		this.st=Status.INIT;
-	}
-	public User() {
-		this.st=Status.INIT;
-	}
+    private String name;
+    private String phoneNumber;
+    private Status st;
+    public User(String name, String phoneNumber, Status st) {
+        super();
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.st = st;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public Status getSt() {
+        return st;
+    }
+    public void setSt(Status st) {
+        this.st = st;
+    }
+    public User(String name, String phoneNumber) {
+        super();
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.st=Status.INIT;
+    }
+    public User() {
+        this.st=Status.INIT;
+    }
 	
 }

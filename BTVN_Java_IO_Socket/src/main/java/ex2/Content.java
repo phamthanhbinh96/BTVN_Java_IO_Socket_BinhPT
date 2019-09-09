d8a128fc5bbbb4b48c7a9a536a66122ca860265a
phamthanhbinh96 <54532614+phamthanhbinh96@users.noreply.github.com>
2019-09-09 23:49:24 +0700

    Update Content.java

--- BTVN_Java_IO_Socket/src/main/java/ex2/Content.java
+++ BTVN_Java_IO_Socket/src/main/java/ex2/Content.java
@@ -10,35 +10,32 @@ package ex2;
  * @author P.T.B
  */
 public class Content {
-	private short tags;
-	private short length;
-	private String value;
-	public short getTags() {
-		return tags;
-	}
-	public void setTags(short tags) {
-		this.tags = tags;
-	}
-	public short getLength() {
-		return length;
-	}
-	public String getValue() {
-		return value;
-	}
-	public void setValue(String value) {
-		this.value = value;
-	}
-	public Content(short tags, String value) {
-		super();
-		this.tags = tags;
-		this.length =  (short) value.length();
-		this.value = value;
-	}
-	@Override
-	public String toString() {
-		return "Content [tags=" + tags + ", length=" + length + ", value=" + value + "]";
-	}
-	
-	
-	
+    private short tags;
+    private short length;
+    private String value;
+    public short getTags() {
+        return tags;
+    }
+    public void setTags(short tags) {
+        this.tags = tags;
+    }
+    public short getLength() {
+        return length;
+    }
+    public String getValue() {
+        return value;
+    }
+    public void setValue(String value) {
+        this.value = value;
+    }
+    public Content(short tags, String value) {
+        super();
+        this.tags = tags;
+        this.length =  (short) value.length();
+        this.value = value;
+    }
+    @Override
+    public String toString() {
+        return "Content [tags=" + tags + ", length=" + length + ", value=" + value + "]";
+    }	
 }
